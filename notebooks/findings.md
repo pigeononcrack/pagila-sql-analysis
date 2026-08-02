6fcacb06f3590ed184de20098c1f68da5a471b05
pigeononcrack <omwho12@gmail.com>
2026-08-02 16:33:08 +0300

    add more explorational sql queries

---
+++ notebooks/findings.md
@@ -0,0 +1,11 @@
+- there's 500 stores, only two stores(ID 1 and ID 2) offer movies for rent - 2270 and 2311 copies
+- there's 241 copies that haven't been returned with 182 copies unreturned for 4.5 years. they were rented at the same day and at the same time February 14th, 2022 at 17:16:03+02, not sure for now what is the problem with this.
+- in film table there's rental_duration column and I wasn't sure what it's stands for. So I've checked my hyphothesis if it is allowed duration of rent for this specific film and, based on average time every movie was rented, it seems that my hyphothesis is correct
+- theres 16 categories of movies in database. all of the movies and copies are spread fairly evenly across 16 categories. Revenue is also relatively balanced, averaging around 25,000 per category.
+- The language of movies, revenue and amount of rents don't show any interesting data. the percentage for each is directly proportional to the number of movies. Movie language does not appear to influence rental frequency or revenue. The distribution of rentals and revenue closely follows the distribution of inventory copies across languages.
+- in database there are 999 customers. two of them haven't rented anything. Majority has numerous rents of movies on them. The highest amount of rents for one customer is 97.
+- discovered that most of the customers do not return movies in time
+- found out the overall replacement cost of movies that weren't returned - 4773
+- there's 215 unique movies that wasn't returned
+- the amounts of not returned movies by category is about equall
+- on average there's 750 rents per month(excluding late spring and summer of 2022, when there were unusually large amount of rents)
--- sql/exploration.sql
+++ sql/exploration.sql
@@ -1,16 +1,17 @@
 -- Number of inventory copies per store
 SELECT 
 	address.address AS Store_Address,
+	store.store_id  AS Store_ID,
 	COUNT(inventory.inventory_id) AS Inventory_Count
 FROM store
 
 LEFT JOIN inventory
 ON store.store_id = inventory.store_id
 
-INNER JOIN address
+LEFT JOIN address
 ON store.address_id = address.address_id
 
-GROUP BY address.address_id
+GROUP BY address.address_id,store.store_id
 ORDER BY Inventory_Count DESC;
 
 --HAVEN'T BEEN RETURNED
@@ -88,3 +89,170 @@ LEFT JOIN payment
 
 GROUP BY category.name
 ORDER BY Earned DESC;
+
+--movie language
+SELECT
+	film.title AS Movie,
+	l1.name AS Movie_Language,
+	l2.name AS Original_Language
+FROM film
+
+LEFT JOIN language l1
+	ON film.language_id = l1.language_id
+LEFT JOIN language l2
+	ON film.original_language_id = l2.language_id;
+
+--language copies
+SELECT
+	l.name,
+	COUNT(inventory.inventory_id) AS Language_Copies,
+	ROUND((100.0 * COUNT(inventory.inventory_id) / (SELECT COUNT(inventory_id) FROM inventory)),2) AS Language_Percentage
+FROM "language" l
+
+LEFT JOIN film
+	ON l.language_id = film.language_id
+
+LEFT JOIN inventory
+	ON film.film_id = inventory.film_id
+
+GROUP BY l.name;
+
+--Correlation between amounts of copies of movies by language
+SELECT
+	l.name AS Language,
+	COUNT(DISTINCT inventory.inventory_id) AS Language_Copies,
+	ROUND((100.0 * COUNT( DISTINCT inventory.inventory_id) / (SELECT COUNT(inventory_id) FROM inventory)),2) AS Language_Percentage,
+	SUM(payment.amount) AS Revenue,
+	ROUND((100.0 * SUM(payment.amount) / (SELECT SUM(amount) FROM payment)),2) AS Revenue_Percentage,
+	COUNT(DISTINCT rental.rental_id) AS Amount_Of_Rants,
+	ROUND((100.0 * COUNT(DISTINCT rental.rental_id) / (SELECT COUNT(rental_id) FROM rental)),2) AS Rental_Percentage
+FROM "language" l
+
+LEFT JOIN film
+	ON l.language_id = film.language_id
+
+LEFT JOIN inventory
+	ON film.film_id = inventory.film_id
+
+LEFT JOIN rental
+	ON inventory.inventory_id = rental.inventory_id
+
+LEFT JOIN payment
+	ON rental.rental_id = payment.rental_id
+
+GROUP BY l.language_id;
+
+--customers by amount of rents and overall payments of theirs
+SELECT
+	CONCAT(customer.first_name || ' ' || customer.last_name) AS Customer_Name,
+	COUNT (rental.rental_id) AS Rents_Amount,
+	SUM(payment.amount) AS Revenue
+FROM customer
+
+LEFT JOIN rental
+	ON customer.customer_id = rental.customer_id
+
+LEFT JOIN payment
+	ON rental.rental_id = payment.rental_id
+
+GROUP BY customer.customer_id
+ORDER BY Rents_Amount DESC;
+
+--percentages of people returning rented movies in time
+SELECT 
+	CONCAT(customer.first_name || ' ' || customer.last_name) AS Customer_Name,
+	COUNT (rental.rental_id) AS Rents_Amount,
+	SUM(CASE
+			WHEN rental.return_date IS NOT NULL AND
+			((EXTRACT(EPOCH FROM (rental.return_date - rental.rental_date))) / 86400) <= film.rental_duration THEN 1
+			ELSE 0
+		END) AS In_Time,
+	
+	ROUND((SUM(CASE
+			WHEN rental.return_date IS NOT NULL AND
+			((EXTRACT(EPOCH FROM (rental.return_date - rental.rental_date))) / 86400) <= film.rental_duration THEN 1
+			ELSE 0
+		END)) * 100.0 / NULLIF(COUNT(rental.rental_id), 0),2) AS Percentage_In_Time
+FROM customer
+
+LEFT JOIN rental
+ON customer.customer_id = rental.customer_id
+
+LEFT JOIN inventory
+ON rental.inventory_id = inventory.inventory_id
+
+LEFT JOIN film
+ON inventory.film_id = film.film_id
+
+GROUP BY customer.customer_id
+ORDER BY Percentage_In_Time DESC;
+
+--each movie missing with amount and sum of replacement cost
+SELECT 
+	film.title AS Movie,
+	COUNT(film.film_id) AS Number_Of_Missing,
+	SUM(film.replacement_cost) AS Replacement_Cost
+FROM rental
+
+INNER JOIN inventory
+	ON rental.inventory_id = inventory.inventory_id
+
+INNER JOIN film
+	ON inventory.film_id = film.film_id
+
+
+
+WHERE rental.return_date IS NULL
+GROUP BY film.film_id
+ORDER BY Number_Of_Missing DESC;
+
+--OVERALL replacement cost
+SELECT SUM(Replacement_Cost) FROM
+(SELECT 
+	film.title AS Movie,
+	COUNT(film.film_id) AS Number_Of_Missing,
+	SUM(film.replacement_cost) AS Replacement_Cost
+FROM rental
+
+INNER JOIN inventory
+	ON rental.inventory_id = inventory.inventory_id
+
+INNER JOIN film
+	ON inventory.film_id = film.film_id
+
+
+
+WHERE rental.return_date IS NULL
+GROUP BY film.film_id
+ORDER BY Number_Of_Missing DESC);
+
+
+--missing by category
+
+SELECT 
+	category.name AS Category,
+	COUNT(rental.rental_id) AS Number_Of_Missing
+	--SUM(film.replacement_cost) AS Replacement_Cost
+FROM rental
+
+left JOIN inventory
+	ON rental.inventory_id = inventory.inventory_id
+
+left JOIN film_category
+	ON inventory.film_id = film_category.film_id
+
+left JOIN 	category
+	ON film_category.category_id = category.category_id
+
+WHERE rental.return_date IS NULL
+GROUP BY category.category_id
+ORDER BY Number_Of_Missing DESC;
+
+--rents by month
+SELECT 
+	DATE_TRUNC('month', rental_date) AS Rent_Month,
+	COUNT(rental_id) AS Amount
+FROM rental
+
+GROUP BY DATE_TRUNC('month', rental_date)
+ORDER BY Rent_Month;
